bd30275ef74e4a900d04d5e6d152e191a6cafe54
anurag33 <45606134+anurag33@users.noreply.github.com>
2022-07-12 10:23:59 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,5 @@
-class Simple{
-  public static void main(String args[]) {
-    
-    System.out.println("Welcome to the Jenkins Tutorial");
-  }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
